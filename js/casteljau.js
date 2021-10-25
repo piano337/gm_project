9f87fa13d25645bab700c2fb6a395772f6db45f4
SlavaStalin <juansebasmontes@gmail.com>
2021-10-25 16:56:11 +0200

    Visual stuff more or less implemented

--- js/casteljau.js
+++ js/casteljau.js
@@ -1,8 +1,12 @@
-
+//TODO Cleaning code, adding constants, but seems to work :D
 //PoC of Casteljau's Algorithm, INPUT = Control points and parameter t, OUTPUT= Bezier curve.
 
-
 const POINTS = [];
+
+const COLOR_BEZIER_CURVE = 0; // 0 means 'black'
+const COLOR_CANVAS = 'linen';
+const COLOR_POINTS = 0; // 0 means 'black'
+
 
 
 function addControlPoint(x,y){
@@ -11,43 +15,59 @@ function addControlPoint(x,y){
 }
 
 function setup() {
-	addControlPoint(1,2);
 
-	addControlPoint(5,2);
+	//Setting up Canvas
+	let myCanvas = createCanvas(600, 400);
+	myCanvas.parent('canvas-container');
+  
+	//Setting up points	
+	addControlPoint(50,100);
 
-	addControlPoint(6,7);
+	addControlPoint(250,100);
 
-	addControlPoint(9,1);
+	addControlPoint(300,350);
+
+	addControlPoint(450,50);
 	
-	casteljauAlgorithm(0);
-	casteljauAlgorithm(0.1);
 	
-	casteljauAlgorithm(0.2);
+}
+function draw(){
+	background(COLOR_CANVAS);
+	// Draw points
+	for (const p of POINTS) {
+		stroke(COLOR_POINTS);
+		fill(COLOR_POINTS);
+		circle(p.x, p.y, 15);
+	}
+	//Draw Polygon lines
+	stroke(COLOR_BEZIER_CURVE);
+	noFill();
+	beginShape();
+	for (const p of POINTS) {
+		vertex(p.x, p.y);
+	}
+	endShape();
+	//Just to see B-Curve, TODO: something like loading bar
+	for (t = 0 ; t<1; t=t+0.001) {
+		stroke(COLOR_POINTS);
+		fill(COLOR_POINTS);
+		bpoint= casteljauAlgorithm(t)
+		circle(bpoint.x, bpoint.y, 2);
+	}
 
-	casteljauAlgorithm(0.3);
-	casteljauAlgorithm(0.4);
-	casteljauAlgorithm(0.5);
-	casteljauAlgorithm(0.6);
-	casteljauAlgorithm(0.7);
-	casteljauAlgorithm(0.8);
-	casteljauAlgorithm(0.9);
-	casteljauAlgorithm(1);
 }
 
 function casteljauAlgorithm(t){
 	n = POINTS.length
-	newPoints = [...POINTS]
-
+	newPoints = POINTS.slice()
 	for (var r = 1; r <n; r++){
 		
 		for(var j = 0; j< n-r; j++){
-			//X-Coordinate
-			newPoints[j].x = newPoints[j].x * (1-t) + t * newPoints[j+1].x
-			//Y-Coordinate
-			newPoints[j].y = newPoints[j].y*(1-t) + t*newPoints[j+1].y
+			//Need to create a new Vector Object
+			newPoints[j] = createVector(newPoints[j].x * (1-t) + t * newPoints[j+1].x,newPoints[j].y*(1-t) + t*newPoints[j+1].y )
 		}	
 	}
-
-	console.log(newPoints[0])
-
+	//Printing b_0^n i.e the point which traces the B-Curve
+	return newPoints[0]
+	
 }
