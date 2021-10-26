4ec8ec9de4e43629cffbaad18029a2198efebb9f
Amir H. E <piano337@yahoo.de>
2021-10-26 19:02:15 +0200

    Bernstein polynomials almost finished

--- js/bernstein.js
+++ js/bernstein.js
@@ -1,6 +1,17 @@
 const CANVAS_SIZE = 500;
 const GRID_SIZE = CANVAS_SIZE / 4;
 const NUMBER_OF_STEPS = 100;
+const COLORS = [];
+
+let binomial_coefficients;
+
+let color_hue = 0
+function addPoint() {
+  colorMode(HSB);
+  COLORS.push(color(color_hue % 360, 100, 100));
+  color_hue += 30
+  colorMode(RGB);
+}
 
 function pascalsTriangle(n) {
   let line = [1];
@@ -14,27 +25,34 @@ function pascalsTriangle(n) {
 function setup() {
   let myCanvas = createCanvas(CANVAS_SIZE, CANVAS_SIZE);
   myCanvas.parent('canvas-container');
-  console.log(pascalsTriangle(4));
+  output_t = select('#t_value');
+  slider = select('#slider_t_value');
+  slider.input(() => {
+    output_t.html('t = ' + slider.value().toLocaleString(
+      undefined, // leave undefined to use the visitor's browser locale or a string like 'en-US' to override it.
+      { minimumFractionDigits: 2 }
+    ));
+  });
   noLoop();
 }
 
 function draw() {
   background('linen');
+  graphGrid();
+  n = 5;
+  for(let i = 0; i <= n; i++) addPoint();
+  
+  binomial_coefficients = pascalsTriangle(n);
+  functionPlot(n);
   graphAxis();
-  functionPlot();
 }
 
-function graphAxis() {
+function bernstein(n, j, t) {
+  const coefficient = binomial_coefficients[j];
+  return (coefficient * Math.pow(t, j) * Math.pow((CANVAS_SIZE - t), n - j));
+}
 
-  stroke(0);
-  strokeWeight(5);
-
-  // x axis
-  line(0, height, width, height);
-
-  // y axis
-  line(0, 0, 0, height);
-
+function graphGrid(){
   // Grid
   stroke('grey');
   strokeWeight(1);
@@ -48,55 +66,32 @@ function graphAxis() {
   }
 }
 
-function functionPlot() {
+function graphAxis() {
+  translate(0, -height);
+  stroke(0);
+  strokeWeight(5);
+
+  // x axis
+  line(0, height, width, height);
+
+  // y axis
+  line(0, 0, 0, height);
+
+}
+
+function functionPlot(n) {
   translate(0, height);
   strokeWeight(2);
   noFill();
-  stroke('blue');
-  beginShape();
-  let n = 2
-  for (let i = 0; i <= NUMBER_OF_STEPS; i++) {
-    // let x = i * (width / NUMBER_OF_STEPS);
-    let x = i * (CANVAS_SIZE / NUMBER_OF_STEPS);
-    let f_x = 3 * x * Math.pow(CANVAS_SIZE - x, n);
-    let y = (-1) * f_x * (1 / Math.pow(CANVAS_SIZE, n));
-    vertex(x, y);
+  for (let j = 0; j <= n; j++) {
+    stroke(COLORS[j]);
+    beginShape();
+    for (let i = 0; i <= NUMBER_OF_STEPS; i++) {
+      let x = i * (CANVAS_SIZE / NUMBER_OF_STEPS);
+      let f_x = bernstein(n, j, x);
+      let y = (-1) * f_x * (1 / Math.pow(CANVAS_SIZE, n - 1));
+      vertex(x, y);
+    }
+    endShape();
   }
-  endShape();
-
-  noFill();
-  stroke('red');
-  beginShape();
-  for (let i = 0; i <= NUMBER_OF_STEPS; i++) {
-    // let x = i * (width / NUMBER_OF_STEPS);
-    let x = i * (CANVAS_SIZE / NUMBER_OF_STEPS);
-    let f_x = 3 * Math.pow(x, n) * (CANVAS_SIZE - x);
-    let y = (-1) * f_x * (1 / Math.pow(CANVAS_SIZE, n));
-    vertex(x, y);
-  }
-  endShape();
-
-  noFill();
-  stroke('green');
-  beginShape();
-  for (let i = 0; i <= NUMBER_OF_STEPS; i++) {
-    // let x = i * (width / NUMBER_OF_STEPS);
-    let x = i * (CANVAS_SIZE / NUMBER_OF_STEPS);
-    let f_x = Math.pow(x, n);
-    let y = (-1) * f_x * (1 / Math.pow(CANVAS_SIZE, n-1));
-    vertex(x, y);
-  }
-  endShape();
-
-  noFill();
-  stroke('black');
-  beginShape();
-  for (let i = 0; i <= NUMBER_OF_STEPS; i++) {
-    // let x = i * (width / NUMBER_OF_STEPS);
-    let x = i * (CANVAS_SIZE / NUMBER_OF_STEPS);
-    let f_x = Math.pow((CANVAS_SIZE - x), n)
-    let y = (-1) * f_x * (1 / Math.pow(CANVAS_SIZE, n-1));
-    vertex(x, y);
-  }
-  endShape();
 }
