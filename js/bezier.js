c3d01004c87f48bbbd87020509de0c55f6cd9d92
Amir H. E <piano337@yahoo.de>
2021-10-29 22:09:13 +0200

    OMG... What a work... but it works :exploding_head:

--- js/bezier.js
+++ js/bezier.js
@@ -24,213 +24,223 @@ let bezier_curve = [];
 let bool_adding = false;
 let bool_found = false;
 
-// Function to add a single point with coordinates (x,y) to the POINTS-list and
-// calculating a new color based only on the hue value as defined in the HSB color model.
-let color_hue = 0;
-function addPoint(x, y) {
-  if (x <= CANVAS_WIDTH && y <= CANVAS_HEIGHT) {
-    number_of_points++;
-    POINTS.push(createVector(x, y));
-    colorMode(HSB);
-    COLORS.push(color(color_hue % 360, 100, 100));
-    color_hue += 20;
-    colorMode(RGB);
-  }
-}
+let bezier_sketch = function (p) {
 
-function clearBezierCurve() {
-  bezier_curve.splice(0);
-  slider.value("0");
-}
-
-// Setting up the canvas, the buttons and the slider.
-function setup() {
-  let myCanvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  myCanvas.parent("bezier-canvas-container");
-  addPoint(100, 130);
-  addPoint(230, 120);
-  addPoint(300, 300);
-  addPoint(530, 320);
-
-  output_t = select("#t_value");
-
-  slider = select("#slider_t_value");
-  slider.input(() => {
+  p.clearBezierCurve = function() {
+    bezier_curve.splice(0);
+    slider.value("0");
     output_t.html(
       "t = " +
-      slider.value().toLocaleString(
+      (slider.value() / NUMBER_OF_STEPS).toLocaleString(
         undefined, // leave undefined to use the visitor's browser locale or a string like 'en-US' to override it.
         { minimumFractionDigits: 2 }
       )
     );
-  });
+  };
 
-  btn_add = select("#button_add");
-  btn_add.mousePressed(() => {
-    bool_adding = true;
-    btn_add.style("background-color", "#999");
-  });
-
-  select("#button_del").mousePressed(() => {
-    if (number_of_points > 2) {
-      number_of_points--;
-      POINTS.pop();
-      clearBezierCurve();
-    }
-  });
-  deCasteljau();
-  noLoop();
-}
-
-// Catching mouse pressed events:
-// If "ADD" button was pressed before, set a new point and clear the Bézier curve,
-// else go for moving a point, i.e. mouse drag event.
-function mousePressed() {
-  if (bool_adding) {
-    if (mouseX <= CANVAS_WIDTH && mouseY <= CANVAS_HEIGHT) {
-      addPoint(mouseX, mouseY);
-      bool_adding = false;
-      clearBezierCurve();
-      btn_add.removeAttribute("style");
-      deCasteljau();
-    }
-  } else {
-    for (const p of POINTS) {
-      const d = dist(p.x, p.y, mouseX, mouseY);
-      if (d < POINT_RADIUS) {
-        point_to_move = p;
-        bool_found = true;
-        break;
+  p.deCasteljau = function () {
+    for (let t_i = 0; t_i <= NUMBER_OF_STEPS; t_i++) {
+      const t = (1.0 * t_i) / NUMBER_OF_STEPS;
+      let current_points = POINTS;
+      for (let i = 0; i < number_of_points - 1; i++) {
+        const helper_points = [];
+        for (let j = 0; j < current_points.length - 1; j++) {
+          // Linear interpolation
+          vec = p.createVector(
+            current_points[j].x * (1 - t) + t * current_points[j + 1].x,
+            current_points[j].y * (1 - t) + t * current_points[j + 1].y
+          );
+          helper_points.push(vec);
+        }
+        current_points = helper_points;
+        if (i >= number_of_points - 2) {
+          bezier_curve.push(current_points[0]);
+        }
       }
     }
-    if (bool_found) {
-      clearBezierCurve();
+  };
+  // Function to add a single point with coordinates (x,y) to the POINTS-list and
+  // calculating a new color based only on the hue value as defined in the HSB color model.
+  let color_hue = 0;
+  p.addPoint = function (x, y) {
+    if (x <= CANVAS_WIDTH && y <= CANVAS_HEIGHT) {
+      number_of_points++;
+      POINTS.push(p.createVector(x, y));
+      p.colorMode(p.HSB);
+      COLORS.push(p.color(color_hue % 360, 100, 100));
+      color_hue += 20;
+      p.colorMode(p.HSB);
     }
-    deCasteljau();
-  }
-  redraw();
-}
+  };
+  // Setting up the canvas, the buttons and the slider.
+  p.setup = function () {
 
-// If we are not adding a point, we maybe dragging a point to move it... :)
-function mouseDragged() {
-  if (!bool_adding) {
-    if (point_to_move) {
-      point_to_move.set(mouseX, mouseY);
+    let bezierCanvas = p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    bezierCanvas.parent("bezier-canvas-container");
+    p.addPoint(100, 130);
+    p.addPoint(230, 120);
+    p.addPoint(300, 300);
+    p.addPoint(530, 320);
+
+    output_t = p.select("#t_value");
+
+    slider = p.select("#slider_t_value");
+    slider.input(() => {
+      output_t.html(
+        "t = " +
+        (slider.value() / NUMBER_OF_STEPS).toLocaleString(
+          undefined, // leave undefined to use the visitor's browser locale or a string like 'en-US' to override it.
+          { minimumFractionDigits: 2 }
+        )
+      );
+    });
+
+    btn_add = p.select("#button_add");
+    btn_add.mousePressed(() => {
+      bool_adding = true;
+      btn_add.style("background-color", "#999");
+    });
+
+    p.select("#button_del").mousePressed(() => {
+      if (number_of_points > 2) {
+        number_of_points--;
+        POINTS.pop();
+        p.clearBezierCurve();
+      }
+    });
+    p.deCasteljau();
+    p.noLoop();
+  };
+
+  // Catching mouse pressed events:
+  // If "ADD" button was pressed before, set a new point and clear the Bézier curve,
+  // else go for moving a point, i.e. mouse drag event.
+  p.mousePressed = function () {
+    if (bool_adding) {
+      if (p.mouseX <= CANVAS_WIDTH && p.mouseY <= CANVAS_HEIGHT) {
+        p.addPoint(p.mouseX, p.mouseY);
+        bool_adding = false;
+        p.clearBezierCurve();
+        btn_add.removeAttribute("style");
+        p.deCasteljau();
+      }
+    } else {
+      for (const point of POINTS) {
+        const d = p.dist(point.x, point.y, p.mouseX, p.mouseY);
+        if (d < POINT_RADIUS) {
+          point_to_move = point;
+          bool_found = true;
+          break;
+        }
+      }
+      if (bool_found) {
+        p.clearBezierCurve();
+      }
+      p.deCasteljau();
     }
-    if (bool_found) {
-      clearBezierCurve();
+    p.redraw();
+  };
+
+  // If we are not adding a point, we maybe dragging a point to move it... :)
+  p.mouseDragged = function () {
+    if (!bool_adding) {
+      if (point_to_move) {
+        point_to_move.set(p.mouseX, p.mouseY);
+      }
+      if (bool_found) {
+        p.clearBezierCurve();
+      }
     }
-  }
-  redraw();
-}
+    p.redraw();
+  };
 
-function mouseReleased() {
-  if (!bool_adding) {
-    point_to_move = null;
-    bool_found = false;
-  }
-  deCasteljau();
-  redraw();
-}
+  p.mouseReleased = function () {
+    if (!bool_adding) {
+      point_to_move = null;
+      bool_found = false;
+    }
+    p.deCasteljau();
+    p.redraw();
+  };
 
-function deCasteljau() {
-  for (let t_i = 0; t_i <= NUMBER_OF_STEPS; t_i++) {
-    const t = (1.0 * t_i) / NUMBER_OF_STEPS;
+  // Drawing function
+  // TODO:
+  // - Separate the logic from the view: separate the calculation of the Bézier curve from the visualization of it!
+  // - Creating the Bernstein polynomials with CindyJS or with p5.js.
+  p.draw = function () {
+    p.background(COLOR_CANVAS);
+
+    // Draw points
+    for (const point of POINTS) {
+      p.stroke(COLOR_POINTS);
+      p.fill(COLOR_POINTS);
+      p.circle(point.x, point.y, POINT_DIAMETER);
+    }
+
+    // Draw control polygon
+    p.stroke(COLOR_BEZIER_CURVE);
+    p.noFill();
+    p.beginShape();
+    for (const point of POINTS) {
+      p.vertex(point.x, point.y);
+    }
+    p.endShape();
+
     let current_points = POINTS;
     for (let i = 0; i < number_of_points - 1; i++) {
       const helper_points = [];
+
       for (let j = 0; j < current_points.length - 1; j++) {
+        t = (1.0 * slider.value()) / NUMBER_OF_STEPS;
         // Linear interpolation
-        vec = createVector(
+        vec = p.createVector(
           current_points[j].x * (1 - t) + t * current_points[j + 1].x,
           current_points[j].y * (1 - t) + t * current_points[j + 1].y
         );
         helper_points.push(vec);
       }
+
       current_points = helper_points;
-      if (i >= number_of_points - 2) {
-        bezier_curve.push(current_points[0]);
+
+
+      // Draw helper points
+      for (const v of helper_points) {
+        // Draw point on Bézier curve...
+        if (i == number_of_points - 2) {
+          p.stroke(COLOR_BEZIER_CURVE);
+          p.fill(COLOR_BEZIER_CURVE);
+          p.circle(v.x, v.y, POINT_DIAMETER);
+        }
+        // ... else draw smaller points as helper points
+        else {
+          p.stroke(COLORS[i]);
+          p.fill(COLORS[i]);
+          p.circle(v.x, v.y, POINT_RADIUS);
+        }
       }
+
+      // Draw helper lines
+      p.stroke(COLORS[i]);
+      p.noFill();
+      p.beginShape();
+      for (const v of helper_points) {
+        p.vertex(v.x, v.y);
+      }
+      p.endShape();
     }
-  }
+
+    // Draw Bézier curve
+    p.stroke(COLOR_BEZIER_CURVE);
+    p.strokeWeight(STROKE_WEIGHT_BEZIER_CURVE);
+    p.noFill();
+    p.beginShape();
+    for (let i = 0; i <= slider.value(); i++) {
+      let point = bezier_curve[i];
+      p.vertex(point.x, point.y);
+    }
+    p.endShape();
+    p.strokeWeight(STROKE_WEIGHT_HELPER_LINES);
+  };
 }
 
-// Drawing function
-// TODO:
-// - Separate the logic from the view: separate the calculation of the Bézier curve from the visualization of it!
-// - Creating the Bernstein polynomials with CindyJS or with p5.js.
-function draw() {
-  background(COLOR_CANVAS);
-
-  // Draw points
-  for (const p of POINTS) {
-    stroke(COLOR_POINTS);
-    fill(COLOR_POINTS);
-    circle(p.x, p.y, POINT_DIAMETER);
-  }
-
-  // Draw control polygon
-  stroke(COLOR_BEZIER_CURVE);
-  noFill();
-  beginShape();
-  for (const p of POINTS) {
-    vertex(p.x, p.y);
-  }
-  endShape();
-
-  let current_points = POINTS;
-  for (let i = 0; i < number_of_points - 1; i++) {
-    const helper_points = [];
-
-    for (let j = 0; j < current_points.length - 1; j++) {
-      t = slider.value();
-      // Linear interpolation
-      vec = createVector(
-        current_points[j].x * (1 - t) + t * current_points[j + 1].x,
-        current_points[j].y * (1 - t) + t * current_points[j + 1].y
-      );
-      helper_points.push(vec);
-    }
-
-    current_points = helper_points;
-
-
-    // Draw helper points
-    for (const v of helper_points) {
-      // Draw point on Bézier curve...
-      if (i == number_of_points - 2) {
-        stroke(COLOR_BEZIER_CURVE);
-        fill(COLOR_BEZIER_CURVE);
-        circle(v.x, v.y, POINT_DIAMETER);
-      }
-      // ... else draw smaller points as helper points
-      else {
-        stroke(COLORS[i]);
-        fill(COLORS[i]);
-        circle(v.x, v.y, POINT_RADIUS);
-      }
-    }
-
-    // Draw helper lines
-    stroke(COLORS[i]);
-    noFill();
-    beginShape();
-    for (const v of helper_points) {
-      vertex(v.x, v.y);
-    }
-    endShape();
-  }
-
-  // Draw Bézier curve
-  stroke(COLOR_BEZIER_CURVE);
-  strokeWeight(STROKE_WEIGHT_BEZIER_CURVE);
-  noFill();
-  beginShape();
-  current_t = Math.floor(slider.value() * NUMBER_OF_STEPS);
-  for (let i = 0; i <= current_t; i++) {
-    let p = bezier_curve[i];
-    vertex(p.x, p.y);
-  }
-  endShape();
-  strokeWeight(STROKE_WEIGHT_HELPER_LINES);
-}
+let bezier_p5 = new p5(bezier_sketch);
